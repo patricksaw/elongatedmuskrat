714c9c3b1dd079ad86fb32ee096c1ff1fb4f8e84
Patrick Saweikis <patsaw@gmail.com>
2022-11-04 11:57:27 -0500

    Remove message, fix static hosting

--- src/cache.ts
+++ src/cache.ts
@@ -1,13 +1,15 @@
 const ONE_HOUR = 60 * 60
 const ONE_DAY = 24 * ONE_HOUR
 const ONE_WEEK = ONE_DAY * 7
+const ONE_YEAR = ONE_WEEK * 52
 
 /**
  * The default cache setting for pages in the shopping flow
  */
 export const CACHE_PAGES = {
     edge: {
-        maxAgeSeconds: ONE_WEEK,
+        maxAgeSeconds: ONE_YEAR,
+        staleWhileRevalidateSeconds: ONE_YEAR
     },
     browser: {
         maxAgeSeconds: 0,
@@ -20,7 +22,8 @@ export const CACHE_PAGES = {
  */
 export const CACHE_ASSETS = {
     edge: {
-        maxAgeSeconds: ONE_WEEK,
+        maxAgeSeconds: ONE_YEAR,
+        staleWhileRevalidateSeconds: ONE_YEAR,
     },
     browser: {
         maxAgeSeconds: 0,
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from '@layer0/core/router'
+const { BACKENDS } = require('@layer0/core/constants');
 
 import { CACHE_PAGES } from './cache'
 
-export default new Router()
-  .get('/:path*', ({ cache, serveStatic }) => {
-    cache(CACHE_PAGES)
-    serveStatic('musk/:path*')
-  })
+export default new Router().static('musk')
+
+  // .get('/:path*', ({ cache, serveStatic, compute, proxy }) => {
+  //   cache(CACHE_PAGES)
+  //   compute(({ path }) => {
+  //     proxy(BACKENDS.static, {path: 'musk/:path*' })
+  //   })
+    
+  //})
